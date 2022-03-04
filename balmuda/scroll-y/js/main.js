9e2f0b2b21f5a1cbd8489adb92254aa956d15c55
구인경 <gu-ingyeong@gu-ingyeong-ui-MacBookPro.local>
2022-03-04 14:38:05 +0900

    0304업데이트

--- balmuda/scroll-y/js/main.js
+++ balmuda/scroll-y/js/main.js
@@ -125,6 +125,120 @@ window.addEventListener('DOMContentLoaded',
         });
 
 
+
+
+        $("#sc05_slider").slick({
+            dots: false, //navigation
+            arrows: true, //arrow
+            prevArrow: $('#section05 .prev'), //prev
+            nextArrow: $('#section05 .next'), //next
+            autoplay: false, // autoplay mode
+            autoplaySpeed: 3000, // auto speed
+            pauseOnHover: false, // pause on mouse hover
+            fade: false, //fade mode only one slider
+            speed: 900, // speed
+            infinite: true, // infinite mode
+            // asNavFor: '.slider2', // another slider
+            centerMode: false, // center move
+            centerPadding: '0%', // center move padding
+            slidesToShow: 1, // show slider number
+            slidesToScroll: 1, // next slider number
+            swipe: true, // swiper
+            focusOnSelect: true, // click to slider
+            draggable: true,
+            vertical: false, // vertical slider
+            verticalSwiping: false, // vertical swiper
+            initialSlide: 0,// slider number
+            cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)', //css transition∑
+            variableWidth: false,
+
+        });
+
+
+
+
+        $("#main_slider").slick({
+            dots: true, //navigation
+            arrows: true, //arrow
+            prevArrow: $('#main_slider .prev'), //prev
+            nextArrow: $('#main_slider .next'), //next
+            autoplay: false, // autoplay mode
+            autoplaySpeed: 2000, // auto speed
+            pauseOnHover: false, // pause on mouse hover
+            fade: false, //fade mode only one slider
+            speed: 900, // speed
+            infinite: true, // infinite mode
+            // asNavFor: '.slider2', // another slider
+            centerMode: false, // center move
+            centerPadding: '0%', // center move padding
+            slidesToShow: 1, // show slider number
+            slidesToScroll: 1, // next slider number
+            swipe: true, // swiper
+            focusOnSelect: true, // click to slider
+            draggable: true,
+            vertical: false, // vertical slider
+            verticalSwiping: false, // vertical swiper
+            initialSlide: 0,// slider number
+            cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)', //css transition∑
+            variableWidth: false,
+
+        });
+
+        $("#visual_slide").slick({
+            dots: true, //navigation
+            arrows: true, //arrow
+            prevArrow: $('#visual_slider .prev'), //prev
+            nextArrow: $('#visual_slider .next'), //next
+            autoplay: false, // autoplay mode
+            autoplaySpeed: 2000, // auto speed
+            pauseOnHover: false, // pause on mouse hover
+            fade: false, //fade mode only one slider
+            speed: 900, // speed
+            infinite: true, // infinite mode
+            // asNavFor: '.slider2', // another slider
+            centerMode: false, // center move
+            centerPadding: '0%', // center move padding
+            slidesToShow: 1, // show slider number
+            slidesToScroll: 1, // next slider number
+            swipe: true, // swiper
+            focusOnSelect: true, // click to slider
+            draggable: true,
+            vertical: false, // vertical slider
+            verticalSwiping: false, // vertical swiper
+            initialSlide: 0,// slider number
+            cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)', //css transition∑
+            variableWidth: false,
+
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     })
 
 
