1874f00837251996b11b47a3dd5d2a4a44c64467
kooinkyung <rndlsrud0298@naver.com>
2022-03-09 17:12:33 +0900

    0309작업

--- pp/js/main.js
+++ pp/js/main.js
@@ -1,39 +1,102 @@
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('DOMContentLoaded',
+    function () {
 
 
+        /* scroll event section */
+        container.addListener((e) => {
 
-    /* scroll event section */
-    container.addListener((e) => {
-
-        let scrollTop = container.scrollTop;
-        $('.posNum').html(scrollTop)
-
+            let scrollTop = container.scrollTop;
+            $('.posNum').html(scrollTop)
 
 
+            if (scrollTop >= 300) {
+                $('#section02').addClass('on');
+            }
+
+            if (scrollTop >= 1900) {
+                $('#section03').addClass('on');
+            }
 
 
-
-
-        
-        
-
-
-
-
-
-
+        });
 
 
 
 
-    });
+
+        /*  slide,click event section */
 
 
 
-    /*  slide,click event section */
 
 
 
-})
+
+
+
+
+
+
+
+
+
+
+
+
+        $('#text_box03').mouseenter(function () {
+            $('#text_box03 .text02').css('width', '74px');
+            $('#text_box03 .text04').css('width', '74px');
+        });
+
+        $('#text_box03 .contents').mouseleave(function () {
+            $('#text_box03 .text02').css('width', '0px');
+            $('#text_box03 .text04').css('width', '0px');
+        });
+
+
+        $('#text_box04').mouseenter(function () {
+            $('#text_box04 .text02').css('width', '119px');
+            $('#text_box04 .text04').css('width', '119px');
+        });
+
+        $('#text_box04 .contents').mouseleave(function () {
+            $('#text_box04 .text02').css('width', '0px');
+            $('#text_box04 .text04').css('width', '0px');
+        });
+
+
+        $('#text_box05').mouseenter(function () {
+            $('#text_box05 .text02').css('width', '74px');
+            $('#text_box05 .text04').css('width', '119px');
+        });
+
+        $('#text_box05 .contents').mouseleave(function () {
+            $('#text_box05 .text02').css('width', '0px');
+            $('#text_box05 .text04').css('width', '0px');
+        });
+
+
+        $('#text_box01').mouseenter(function () {
+            $('#text_box01 .text02').css('width', '119px');
+            $('#text_box01 .text04').css('width', '74px');
+        });
+
+        $('#text_box01 .contents').mouseleave(function () {
+            $('#text_box01 .text02').css('width', '0px');
+            $('#text_box01 .text04').css('width', '0px');
+        });
+
+        $('#text_box02').mouseenter(function () {
+            $('#text_box02 .text02').css('width', '142px');
+            $('#text_box02 .text04').css('width', '142px');
+        });
+
+        $('#text_box02 .contents').mouseleave(function () {
+            $('#text_box02 .text02').css('width', '0px');
+            $('#text_box02 .text04').css('width', '0px');
+        });
+
+
+    })
 
 
